107e1d532d345fcb3ae1d91931e37ab28572cfa2
Johan Suarez <jhoanseb1992@gmail.com>
2024-05-30 14:53:19 +1000

    add hostaddres

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 
 from dotenv import load_dotenv
 import os
+from numpy import who
 import pandas as pd
+from dash import Dash, dcc, html
+import plotly.express as px
+
 load_dotenv()
 
 env_var = os.getenv("DATA_SOURCE")
@@ -14,4 +18,32 @@ if not os.path.exists(env_var):
 
 df = pd.read_json(env_var)
 
-print(df.head())
+gb_df = df.groupby("RequestPath").count().reset_index()
+
+fig = px.bar(gb_df, y="RequestPath", x="ClientHost")
+
+filtered_df = df[df["RequestPath"].str.contains(r"14032025/", na=False) & (df["RequestPath"].str.len() < 30)]
+
+
+fig2 = px.scatter(filtered_df, y="RequestPath", x="ClientHost")
+
+app = Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    ),
+    dcc.Graph(
+        id='Local',
+        figure=fig2
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
